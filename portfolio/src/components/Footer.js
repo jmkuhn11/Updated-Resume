4af7050ae4b869fe1b4c0316eda76714d7bbfbe0
jmkuhn11 <joshuakuhn2022@u.northwestern.edu>
2023-04-03 02:06:23 -0500

    initial setup

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Footer({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="https://github.com/jmkuhn11"
+
+        //   className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Github
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="https://twitter.com/JoshKuhn3"
+
+        //   className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          Twitter
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Footer;
